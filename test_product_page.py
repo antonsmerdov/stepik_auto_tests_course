870f0614cb6f0273e862c08f0c2c07af72c82649
Anton Smerdov <antonsmerdov@decard.me>
2026-02-20 11:59:44 +0700

    test: parametrize product page tests and xfail offer7 bug

--- test_product_page.py
+++ test_product_page.py
@@ -1,16 +1,27 @@
-from pages.product_page import ProductPage
 import pytest
-
-# Новая ссылка из задания
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+from pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
+# 1. Добавляем декоратор со списком всех 10 ссылок
+@pytest.mark.parametrize('link', [
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+    pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                 marks=pytest.mark.xfail),
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
+])
+# 2. Обязательно передаем link как аргумент в функцию!
+def test_guest_can_add_product_to_basket(browser, link):
+    # 3. Внутри ничего не меняем, логика остается старой
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
-
-    # Эти методы сами скачают название книги ("Coders at Work") и сверят его
     page.should_be_message_about_adding()
     page.should_be_message_basket_total()
